88978bf8676201373b5ef0a29b215c96de772d07
RICHARD <richureshu@gmail.com>
2021-02-04 15:10:30 +0530

    added new testing for preprocessing function

--- FakeNewsDetection/testcase.py
+++ FakeNewsDetection/testcase.py
@@ -2,6 +2,11 @@ import unittest
 from  FakeNewsDetection import fake_news_detection
 from .newfeatures import *
 class TestMethods(unittest.TestCase):
+    def test_prepro(self):
+        X = pandas.DataFrame({"text" : ["From the evidence so far, the COVID-19 virus can be transmitted in ALL AREAS, including areas with hot and humid weather. Regardless of climate, adopt protective measures if you live in, or travel to an area reporting COVID-19. The best way to protect yourself against COVID-19 is by frequently cleaning your hands. By doing this you eliminate viruses that may be on your hands and avoid infection that could occur by then touching your eyes, mouth, and nose."] })
+        data = WordLematization.tokenize(self,X.text)
+        self.assertEqual(data ,[['from', 'the', 'evidence', 'so', 'far', ',', 'the', 'covid-19', 'virus', 'can', 'be', 'transmitted', 'in', 'all', 'areas', ',', 'including', 'areas', 'with', 'hot', 'and', 'humid', 'weather', '.', 'regardless', 'of', 'climate', ',', 'adopt', 'protective', 'measures', 'if', 'you', 'live', 'in', ',', 'or', 'travel', 'to', 'an', 'area', 'reporting', 'covid-19', '.', 'the', 'best', 'way', 'to', 'protect', 'yourself', 'against', 'covid-19', 'is', 'by', 'frequently', 'cleaning', 'your', 'hands', '.', 'by', 'doing', 'this', 'you', 'eliminate', 'viruses', 'that', 'may', 'be', 'on', 'your', 'hands', 'and', 'avoid', 'infection', 'that', 'could', 'occur', 'by', 'then', 'touching', 'your', 'eyes', ',', 'mouth', ',', 'and', 'nose', '.']])
+        self.assertEqual(WordLematization.lemmatizerNew(self ,data) ,["['evidence', 'far', 'virus', 'transmit', 'area', 'include', 'area', 'hot', 'humid', 'weather', 'regardless', 'climate', 'adopt', 'protective', 'measure', 'live', 'travel', 'area', 'report', 'best', 'way', 'protect', 'frequently', 'clean', 'hand', 'eliminate', 'virus', 'may', 'hand', 'avoid', 'infection', 'could', 'occur', 'touch', 'eye', 'mouth', 'nose']"])
     def test_prediction(self):
         fk = fake_news_detection("corona_fake_news.csv")
         title ="Due to the recent outbreak for the Coronavirus (COVID-19) the World Health Organization is giving away vaccine kits. Just pay $4.95 for shipping"
